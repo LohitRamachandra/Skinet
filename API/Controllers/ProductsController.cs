f64e34d8e7fc4f2cc1f7a7c8319fb49b9f7a0a9c
LohitRamachandra <tripurana.lohit@gmail.com>
2022-02-03 10:38:22 +0200

    Applying the migrations and creating the Database at app startup

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,12 +16,12 @@ namespace API.Controllers
     public class ProductsController : Controller
     {
         private readonly ILogger<ProductsController> _logger;
-        private readonly StoreContext _context;
+        private readonly IProductRespository _repo;
 
-        public ProductsController(ILogger<ProductsController> logger, StoreContext context)
+        public ProductsController(ILogger<ProductsController> logger, IProductRespository repo)
         {
             _logger = logger;
-            _context = context;
+            _repo = repo;
         }
 
         // public IActionResult Index()
@@ -37,14 +38,14 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
