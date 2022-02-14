3c7f405bb43007d9328530ffa7d4d2c8375a82fd
LohitRamachandra <tripurana.lohit@gmail.com>
2022-02-14 18:44:19 +0200

    Using the specification methods in the controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -61,7 +61,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _produtsRepo.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            return await _produtsRepo.GetEntityWithSpec(spec);
         }
 
         [HttpGet("brands")]
