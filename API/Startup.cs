095a82f803f87b03bec1c44e9647fa2516fef866
LohitRamachandra <tripurana.lohit@gmail.com>
2022-02-22 16:11:41 +0200

    Serving static content from the API - wwwroot

--- API/Startup.cs
+++ API/Startup.cs
@@ -57,6 +57,8 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
+            
+
             app.UseStaticFiles();
 
             app.UseHttpsRedirection();
